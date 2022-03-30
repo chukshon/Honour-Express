033e85dfae26a91e04b70e728ece4ba470820d70
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-03-30 16:12:40 +0100

    default route

--- app.ts
+++ app.ts
@@ -11,7 +11,7 @@ import usersRouter from './routes/users'
 var app: Application = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, "..", 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
